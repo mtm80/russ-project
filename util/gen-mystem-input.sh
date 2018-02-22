d9b0c5f1d688d39980f1ae15b4be9472feb82a60
Richard Fulop <Richard Fulop>
2018-02-21 19:24:13 -0500

    Automates xml transform to mystem input

---
+++ util/gen-mystem-input.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+from sys import argv
+import lxml.etree as ET
+
+dom = ET.parse("../xml/putin/interviu-amerikanskomu-telekanalu-nbc.xml")
+xslt = ET.parse("gen-mystem-input.xslt")
+transform = ET.XSLT(xslt)
+newdom = transform(dom)
+print(ET.tostring(newdom, pretty_print=True))
