01ba03e9d7a80feae9cd8d5b554211f6f1d653ba
Richard Fulop <Richard Fulop>
2018-03-19 13:16:13 -0400

    Gets MALLET output for 5 topics

--- util/mallet-import.sh
+++ util/mallet-import.sh
@@ -1,1 +1,11 @@
-~/mallet import-dir --input ../temp/*/*/mallet-input.txt --output ../temp/web.mallet --stoplist-file ../research/stop-words.txt --token-regex '[\p{L}\p{M}]+'
+mkdir -p ../temp/mallet-input/
+
+#candidates=(putin zhirinovskii grudinin)
+for c in putin zhirinovskii grudinin
+        do for interview in $(echo ../temp/${c}/*)
+        do
+                interviewName="$(echo $interview | cut -d/ -f4)"
+                cp ${interview}/mallet-input.txt ../temp/mallet-input/${c}-${interviewName}.txt
+        done
+done
+~/mallet-2.0.8/bin/mallet import-dir --keep-sequence --input ../temp/mallet-input --output ../temp/web.mallet --stoplist-file ../research/stop-words.txt --token-regex '[\p{L}\p{M}]+'
--- util/run-mallet.sh
+++ util/run-mallet.sh
@@ -1,1 +1,5 @@
-~/mallet import-dir --input ../temp/*/*/mallet-input.txt --output ../temp/web.mallet --stoplist-file ../research/stop-words.txt --token-regex '[\p{L}\p{M}]+'
+PATH=~/mallet-2.0.8/bin:$PATH
+mallet train-topics --input ../temp/web.mallet --num-topics 5 \
+ --output-state ../temp/topic-state.gz --num-iterations 10 \
+ --xml-topic-report ../temp/topic-report.xml \
+ --output-topic-docs ../temp/topic-doc.xml
