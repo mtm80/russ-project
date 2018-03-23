784151b1c90c81cc7675fb01a7aa95ef3fe7fe20
Richard Fulop <Richard Fulop>
2018-03-23 00:52:06 -0400

    Generate topic models for 10, 20, etc. topics

--- util/run-mallet.sh
+++ util/run-mallet.sh
@@ -1,5 +1,11 @@
-PATH=~/mallet-2.0.8/bin:$PATH
-mallet train-topics --input ../analysis/web.mallet --num-topics 5 \
- --output-state ../analysis/topic-state.gz --num-iterations 10 \
- --xml-topic-report ../analysis/topic-report.xml \
- --output-topic-docs ../analysis/topic-doc.xml
+#!/bin/bash
+
+itercount=('5' '10' '15' '20' '25' '30')
+for i in "${itercount[@]}"
+do
+	echo $i
+	~/mallet-2.0.8/bin/mallet train-topics --input ../analysis/web.mallet --num-topics $i \
+		--output-state ../analysis/topic-state-${i}.gz --num-iterations 100 \
+ 		--xml-topic-report ../analysis/topic-report-${i}.xml \
+		--output-topic-docs ../analysis/topic-doc-${i}.xml
+done
