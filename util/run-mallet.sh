2f51f4f8d841c32665fa0108b4c3a1cb6c73a53f
Richard Fulop <Richard Fulop>
2018-04-17 23:06:13 -0400

    little website mods

--- util/run-mallet.sh
+++ util/run-mallet.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-itercount=('5' '10' '15' '20' '25' '30')
-for i in "${itercount[@]}"
-do
-	echo $i
-	~/mallet-2.0.8/bin/mallet train-topics --input ../analysis/web.mallet --num-topics $i \
-		--output-state ../analysis/topic-state-${i}.gz --num-iterations 100 \
- 		--xml-topic-report ../analysis/topic-report-${i}.xml \
-		--output-topic-docs ../analysis/topic-doc-${i}.xml
-done
+~/mallet-2.0.8/bin/mallet train-topics --input ../analysis/web.mallet --num-topics 30 \
+	--output-state ../analysis/topic-state.gz --num-iterations 100 \
+ 	--xml-topic-report ../analysis/topic-report.xml \
+	--output-topic-docs ../analysis/topic-docs.csv
